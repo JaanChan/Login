6117fa3554a24bb3742f5755d0fbe0fb6e6cc1bf
JaanChan <jaanbg178@gmail.com>
2024-12-02 15:13:51 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  console.log("User Data:", data);
+  fs.appendFileSync("./data/user-data.txt", JSON.stringify(data) + "\n");
+  res.status(200).send("Data received");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
